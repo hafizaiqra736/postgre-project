83c2f0e7095adc92223ac3e06eaadc5817f27594
Hafiza Iqra <kinzii896@gmail.com>
2025-04-18 11:39:13 +0500

    Added CRUD operations with POSTGRESQL integretion

--- main.py
+++ main.py
@@ -1,57 +1,63 @@
 import psycopg2
 
-#connect to postgree database
-
-conn = psycopg2.connect(host="localhost",
-                       port=5433,
-                       database="book_db",
-                       user="postgres",
-                       password="admin123")
-
+conn = psycopg2.connect(
+    host="localhost",
+    port=5433,
+    database="book_db",
+    user="postgres",
+    password="admin123"  
+)
 
 cur = conn.cursor()
-print("Connected Successfully")
+#drop table if exists
+cur.execute("DROP TABLE IF EXISTS books")
 
-#create table
+# Create the books table
 cur.execute("""
-CREATE TABLE IF NOT EXISTS books (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100),
-    author VARCHAR(100),
-    year INT
-);
+    CREATE TABLE books (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100),
+        author VARCHAR(100),
+        year INT
+    )
 """)
 
 conn.commit()
-print("Table created.")
+print("Table created successfully!")
 
-#insert table
-cur.execute("""
-INSERT INTO books (title, author, year)
-VALUES(%s, %s, %s)
-""", ("The Alchemist", "Paulo Coelho", 1988))
+
+
+
+books = [
+    ("Harry Potter", "J.K. Rowling", 1997),
+    ("The Hobbit", "J.R.R. Tolkien", 1937),
+    ("1984", "George Orwell", 1949),
+    ("The Alchemist", "Paulo Coelho", 1988)
+]
+
+cur.executemany("INSERT INTO books (title, author, year) VALUES (%s, %s, %s)", books)
 
 conn.commit()
-print("Book Inserted")
+print("Books inserted successfully!")
 
-#Retrieve data(GET)
 cur.execute("SELECT * FROM books")
 rows = cur.fetchall()
+
 for row in rows:
     print(row)
-    
-#update data
-cur.execute("""
-UPDATE books SET year = %s WHERE title = %s""", (1993, "The Alchemist"))
+
+
+#update
+cur.execute("UPDATE books SET year = 1950 WHERE title = '1984'")
 
 conn.commit()
-print("Book Updated")
+print("Book updated successfully!")
 
-#delete data
-cur.execute("DELETE FROM books WHERE title = %s", ("The Alchemist",))
+#delete
+cur.execute("DELETE FROM books WHERE title = 'The Alchemist'")
+
 conn.commit()
-print("Book deleted.")
+print("Book deleted successfully!")
 
-#close connection
 cur.close()
 conn.close()
